35495b9646360d364110d59925d8d0494a32acdf
Muradov2004 <ami65143@gmail.com>
2024-01-23 22:45:53 +0400

    Add project files.

---
+++ TodoWebService/Controllers/TodoController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoWebService.Models.DTOs;
+using TodoWebService.Models.DTOs.Todo;
+using TodoWebService.Services.Todo;
+
+namespace TodoWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoService _todoService;
+
+        public TodoController(ITodoService todoService)
+        {
+            _todoService = todoService;
+        }
+
+        [HttpGet("get/{id}")]
+        public async Task<ActionResult<TodoItemDto>> Get(int id)
+        {
+            var item = await _todoService.GetTodoItem(id);
+
+
+            return item is not null
+                ? item
+                : NotFound();
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            return await _todoService.DeleteTodo(id);
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<TodoItemDto>> Create([FromBody] CreateTodoItemRequest request)
+        {
+            return await _todoService.CreateTodo(request);
+        }
+
+        [HttpPost("change-status")]
+        public async Task<ActionResult<TodoItemDto>> ChangeStatus([FromBody] ChangeStatusRequest request)
+        {
+            return await _todoService.ChangeTodoItemStatus(request);
+        }
+
+        [HttpGet("get-all/{page}/{pageSize}")]
+        public async Task<ActionResult<List<TodoItemDto>>> GetAll(int page, int pageSize)
+        {
+            return await _todoService.GetAll(page, pageSize);
+        }
+    }
+}
---
+++ TodoWebService/Services/Todo/TodoService.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoWebService.Data;
+using TodoWebService.Models.DTOs;
+using TodoWebService.Models.DTOs.Todo;
+using TodoWebService.Models.Entities;
+
+namespace TodoWebService.Services.Todo
+{
+    public class TodoService : ITodoService
+    {
+        private readonly TodoDbContext _context;
+
+        public TodoService(TodoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TodoItemDto> ChangeTodoItemStatus(ChangeStatusRequest request)
+        {
+            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == request.Id);
+            if (todoItem is not null)
+            {
+                todoItem.IsCompleted = request.IsCompeleted;
+                todoItem.UpdatedTime = DateTime.Now;
+                await _context.SaveChangesAsync();
+                return new(todoItem.Id, todoItem.Text, todoItem.IsCompleted, todoItem.CreatedTime);
+            }
+            else return null!;
+
+        }
+
+        public async Task<TodoItemDto> CreateTodo(CreateTodoItemRequest request)
+        {
+            var todo = new TodoItem() { Text = request.Text, IsCompleted = false, UpdatedTime = DateTime.Now, CreatedTime = DateTime.Now };
+            await _context.TodoItems.AddAsync(todo);
+            await _context.SaveChangesAsync();
+            var lastItem = await _context.TodoItems.OrderBy(t => t.Id).LastAsync();
+            return new TodoItemDto(lastItem.Id, lastItem.Text, lastItem.IsCompleted, lastItem.CreatedTime);
+        }
+
+        public async Task<bool> DeleteTodo(int id)
+        {
+            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
+
+            if (todoItem != null)
+            {
+                _context.TodoItems.Remove(todoItem);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<TodoItemDto>> GetAll(int page, int pageSize)
+        {
+            var todoList = await _context.TodoItems.ToListAsync();
+
+            var startIndex = page * pageSize;
+            var endIndex = startIndex + pageSize;
+
+            endIndex = Math.Min(endIndex, todoList.Count);
+
+            var dtoList = new List<TodoItemDto>();
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                dtoList.Add(new TodoItemDto(
+                    Id: todoList[i].Id,
+                    Text: todoList[i].Text,
+                    IsCompleted: todoList[i].IsCompleted,
+                    CreatedTime: todoList[i].CreatedTime
+                ));
+            }
+
+            return dtoList;
+        }
+
+
+        public async Task<TodoItemDto?> GetTodoItem(int id)
+        {
+            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
+
+            return todoItem is not null
+                ? new TodoItemDto(
+                    Id: todoItem.Id,
+                    Text: todoItem.Text,
+                    IsCompleted: todoItem.IsCompleted,
+                    CreatedTime: todoItem.CreatedTime)
+                : null;
+        }
+    }
+}
